b5a73b7c95359221dd99960ff410d9d91643479f
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-05-13 17:42:35 -0400

    Add files via upload

---
+++ draft 4/src/main/java/com/example/androidfinalproject2020/SetReminder.java
@@ -0,0 +1,42 @@
+package com.example.androidfinalproject2020;
+
+
+import android.app.Notification.Builder;
+import android.app.PendingIntent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SetReminder extends BroadcastReceiver {
+
+    String text_input;
+    int notificationId;
+    NotificationManager notificationManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent mainIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+        notificationId = intent.getIntExtra("notificationId", 0);
+        text_input = intent.getStringExtra("todo");
+
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Builder builder = new Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle("Check your App!")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentText(text_input)
+                .setContentIntent(contentIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        notificationManager.notify(notificationId, builder.build());
+
+    }
+}
